b0008ac846b42a7112ba555b69943ce2ca796637
Flip437 <philippediollot@hotmail.fr>
2021-06-17 19:45:00 +0200

    adding infity scroll to search view"

--- Components/Search_from course.js
+++ Components/Search_from course.js
@@ -2,62 +2,59 @@
 
 import React from 'react'
 import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
-import FilmItem from './FilmItem.js'
+import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
 
-
   constructor(props) {
     super(props)
-    this.state = { 
-      films: [],
-      isLoading: false
-     }
     this.searchedText = ""
     this.page = 0
     this.totalPages = 0
+    this.state = {
+      films: [],
+      isLoading: false
+    }
   }
 
   _loadFilms() {
-    if(this.searchedText.length > 0) {
+    if (this.searchedText.length > 0) {
       this.setState({ isLoading: true })
       getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
-        this.page = data.page
-        this.totalPages = data.total_pages
-        this.setState({ 
-          films: [ ...this.state.films, ...data.results ],//films: this.state.films.concat(data.results)
-          isLoading: false
-        })
-      });
+          this.page = data.page
+          this.totalPages = data.total_pages
+          this.setState({
+            films: [ ...this.state.films, ...data.results ], //films: this.state.films.concat(data.results)
+            isLoading: false
+          })
+      })
     }
   }
 
   _searchTextInputChanged(text) {
-    this.searchedText = text
-  }
-
-  _displayLoading() {
-
-    if (this.state.isLoading) {
-      return (
-        <View style={styles.loading_container}>
-          <ActivityIndicator size="large" color="#0000ff" />
-        </View>
-      )
-    }
+    this.searchedText = text 
   }
 
   _searchFilms() {
-    // Ici on va remettre à zéro les films de notre state
     this.page = 0
     this.totalPages = 0
     this.setState({
       films: [],
     }, () => {
-      console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
-      this._loadFilms()
+        console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+        this._loadFilms()
     })
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
   }
 
   render() {
@@ -69,7 +66,6 @@ class Search extends React.Component {
           onChangeText={(text) => this._searchTextInputChanged(text)}
           onSubmitEditing={() => this._searchFilms()}
         />
-        {this._displayLoading()}
         <Button title='Rechercher' onPress={() => this._searchFilms()}/>
         <FlatList
           data={this.state.films}
@@ -77,17 +73,15 @@ class Search extends React.Component {
           renderItem={({item}) => <FilmItem film={item}/>}
           onEndReachedThreshold={0.5}
           onEndReached={() => {
-            if (this.page < this.totalPages) {
-              this._loadFilms()
-            }
+              if (this.page < this.totalPages) {
+                 this._loadFilms()
+              }
           }}
         />
-
+        {this._displayLoading()}
       </View>
     )
   }
-
-
 }
 
 const styles = StyleSheet.create({
@@ -96,22 +90,12 @@ const styles = StyleSheet.create({
     marginTop: 20
   },
   textinput: {
-    height: 150,
     marginLeft: 5,
     marginRight: 5,
     height: 50,
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: 'red',
-    padding: 10
-  },
-  text : {
-    fontSize: 20,
-    color: 'red'
   },
   loading_container: {
     position: 'absolute',
