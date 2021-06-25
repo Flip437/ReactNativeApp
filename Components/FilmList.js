659e1864b4f5add9ed5f6e45e8389ef4b3208225
Flip437 <philippediollot@hotmail.fr>
2021-06-25 14:24:08 +0200

    first native app commit

---
+++ Components/FilmList.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { StyleSheet, FlatList } from 'react-native'
+import { connect } from 'react-redux'
+import FilmItem from './FilmItem'
+
+class FilmList extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state= {
+      films: []
+    }
+  }
+
+  _displayDetailForFilm = (idFilm) => {
+    console.log("Display film " + idFilm)
+    // On a récupéré les informations de la navigation, on peut afficher le détail du film
+    this.props.navigation.navigate('Détail des films', {idFilm: idFilm})
+  }
+
+  render(){
+    return (
+      <FlatList
+        style={styles.list}
+        data={this.props.films}
+        extraData={this.props.favoritesFilm} //permet à la flatlist de se re rendre quand une donnée extérieur (qui n'est pas dans sa props data) est modifiée)
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item}) => 
+          <FilmItem
+            film={item}
+            isFilmFavorite={(this.props.favoritesFilm.findIndex(film => film.id === item.id) !== -1) ? true : false}
+            displayDetailForFilm={this._displayDetailForFilm}
+          />
+        }
+        onEndReachedThreshold={0.5}
+        onEndReached={() => {
+          if (!this.props.favoriteList && this.props.page < this.props.totalPages) {
+            console.log("LOAD MORE FILMS FROM FILM LIST COMPONENT")
+            console.log("LOAD MORE FILMS FROM FILM LIST COMPONENT")
+            // On appelle la méthode loadFilm du component Search pour charger plus de films
+            this.props.loadFilms()
+          }
+        }}
+      />
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1
+  }
+})
+
+const mapStateToProps = state => {
+  return {
+    favoritesFilm: state.favoritesFilm
+  }
+}
+
+export default connect(mapStateToProps)(FilmList)
+
---
+++ Navigation/Navigation.js
@@ -0,0 +1,91 @@
+// Navigation/Navigation.js
+
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Search from '../Components/Search'
+import Favorites from '../Components/Favorites'
+import FilmDetail from '../Components/FilmDetail'
+import { StyleSheet, Image } from 'react-native'
+import Test from '../Components/Test'
+
+const Tab = createBottomTabNavigator();
+
+const searchStack = createStackNavigator();
+function searchStackScreen() {
+  return (
+    <searchStack.Navigator>
+      <searchStack.Screen name="Recherche" component={Search} />
+      <searchStack.Screen name="Détail des films" component={FilmDetail} />
+    </searchStack.Navigator>
+  );
+}
+
+
+const favoritesStack = createStackNavigator();
+function favoritesStackScreen() {
+  return (
+    <favoritesStack.Navigator>
+      <favoritesStack.Screen name="Favoris" component={Favorites} />
+      <favoritesStack.Screen name="Détail des films" component={FilmDetail} />
+    </favoritesStack.Navigator>
+  )
+}
+
+
+class Navigation extends React.Component {
+
+  render() {
+    // return (
+    //   <NavigationContainer>
+    //     <Tab.Navigator>
+    //       <Tab.Screen name="Recherche" component={searchStackScreen} />
+    //       <Tab.Screen name="Favoris" component={Favorites} />
+    //     </Tab.Navigator>
+    //   </NavigationContainer>
+    // )
+
+
+
+
+
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: () => {  
+              if (route.name === 'Recherche') {
+                var source = require('../Images/ic_search.png')
+
+              } else if (route.name === 'Favoris') {
+                var source = require('../Images/ic_favorite.png')
+              }
+              // You can return any component that you like here!
+              return <Image source={source} style={styles.icon}/>
+            },
+          })}
+          tabBarOptions={{
+            activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+            inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+            showLabel: false, // On masque les titres
+            showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+          }}
+        >
+          <Tab.Screen name="Test" component={Test} />
+          <Tab.Screen name="Recherche" component={searchStackScreen} />
+          <Tab.Screen name="Favoris" component={favoritesStackScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
+
+export default Navigation
